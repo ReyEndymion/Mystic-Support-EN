77a09d60131b076e38a483125ca56607845bc45a
Rey Endymion <33460024+ReyEndymion@users.noreply.github.com>
2023-04-22 09:18:46 -0600

    preparing the language to english

--- plugins/info-owner.js
+++ plugins/info-owner.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, usedPrefix }) => {
-let name = '👑 Mystic - Creador 👑'    
-let numero = '5219992095479'
+let name = '👑 Mystic - Owner 👑'    
+let numero = [['5219992095479'], ['52117489568']]
 let numeroformato = '+52 1 999 209 5479'
 let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;${name};;;\nFN:${name}\nORG:${name}\nTITLE:\nitem1.TEL;waid=${numero}:${numeroformato}\nitem1.X-ABLabel:${name}\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:${name}\nEND:VCARD`
 await conn.sendMessage(m.chat, { contacts: { displayName: name, contacts: [{ vcard }] }}, {quoted: m})}
--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn }) => {
-let texto1 = `*https://github.com/BrunoSobrino/TheMystic-Bot-MD*`
+let texto1 = `*https://github.com/BrunoSobrino/TheMystic-Bot-MD*\n\n*https://github.com/ReyEndymion/ANI_MX_SCANS-MD*\n\n*https://github.com/ReyEndymion/Mystic-Support-EN*`
 m.reply(texto1)
 }
 handler.command = ['sc','script']
